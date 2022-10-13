6a130e39d1a228f4dcb349e11d8e3831f2980897
Akusterr <109528561+Akusterr@users.noreply.github.com>
2022-10-13 19:25:39 -0400

    updated

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let days = [
   "Saturday"
 ];
 
+const apiKey = "43a017c50cad44b78c7d2a1983f72803";
 const currentDay = days[now.getDay()];
 const time = now.toLocaleTimeString();
 
@@ -30,24 +31,54 @@ function submit(event) {
 const cityForm = document.querySelector("#cityForm");
 cityForm.addEventListener("submit", submit);
 
-function submit(event) {
-  event.preventDefault();
-  let input = document.querySelector("#locationInput");
+const fetchAndRenderCityWeatherData = (str) => {
   let cityHeading = document.querySelector('#locationDisplay');
-  cityHeading.innerHTML = input.value;
-  search(input.value).then((cityWeatherData) => {
+  cityHeading.innerHTML = str;
+  search(str).then((cityWeatherData) => {
     console.log(cityWeatherData)
     let currentTempSpan = document.querySelector('#currentTemp');
     const temp_max = cityWeatherData.main.temp_max;
     const temp_min = cityWeatherData.main.temp_min;
     currentTempSpan.innerHTML = `${temp_min}℃/${temp_max}℃`
+    const weather = cityWeatherData.weather || [];
+    const firstWeater = weather[0] || {};
+    const icon = firstWeater.icon;
+    let iconTarget = document.querySelector('#iconTarget');
+    iconTarget.innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}@2x.png" />`
+
+    const {deg, speed} = cityWeatherData.wind;
+    document.querySelector('#windSpeed').innerHTML = `Speed: ${speed} mph`;
+     document.querySelector('#windDirection').innerHTML = `Direction: ${deg}°`;
   })
+}
+
+const fetchAndRenderDailyWeatherDataForCity = (str) => {
+  searchDailyForcast(str).then((dailyWeatherData) => {
+    console.log('DAILYDATA', dailyWeatherData)
+  })
+}
+
+const updateDailyForcastAndCityWeatherData = (city) => {
+  fetchAndRenderCityWeatherData(city)
+  // fetchAndRenderDailyWeatherDataForCity(city)
+}
+
+function submit(event) {
+  event.preventDefault();
+  let input = document.querySelector("#locationInput");
+  
+  updateDailyForcastAndCityWeatherData(input.value)
 }
 ////////////
 
 function search(city) {
-  let apiKey = "43a017c50cad44b78c7d2a1983f72803";
+  
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  return axios.get(apiUrl).then((result) => result.data);
+}
+
+function searchDailyForcast(city) {
+  let apiUrl = `https://pro.openweathermap.org/data/2.5/forecast/hourly?q=${city}&appid=${apiKey}&units=metric`;
   return axios.get(apiUrl).then((result) => result.data);
 }
 
@@ -60,3 +91,6 @@ function getLocation(event) {
 
 let currentLocation = document.querySelector("#locationDisplay");
 currentLocation.addEventListener("click", getLocation);
+
+
+updateDailyForcastAndCityWeatherData('Belmar');
